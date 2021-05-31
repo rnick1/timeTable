724926fdd94523991358172f41628258df4ab415
rnick1 <rnick0260@gmail.com>
2021-05-31 11:30:27 -0700

    Added jumbotron and started working on the styling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,30 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Jumbotron } from "react-bootstrap";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Jumbotron style={{ border: "solid" }}>
+      <h1
+        style={{
+          textAlign: "center",
+          color: "red",
+          fontSize: "124px",
+        }}
+      >
+        timeTable
+      </h1>
+    </Jumbotron>
+    // {/* // <div className="App">
+    // //   <div className="App-header">
+    // //     <h2>Welcome to React</h2>
+    // //   </div>
+    // //   <p className="App-intro">
+    // //     To get started, edit <code>src/App.js</code> and save to reload.
+    // //   </p>
+    // // </div> */}
+  );
 }
 
 export default App;
