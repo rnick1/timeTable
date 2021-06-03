1a951cbd8ad211ce999dab16a5d187e57d105a17
rnick1 <rnick0260@gmail.com>
2021-06-02 20:05:21 -0700

    Moved styling to app.css.

--- client/src/App.js
+++ client/src/App.js
@@ -6,117 +6,49 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <Jumbotron style={{ border: "solid", width: "100vw" }}>
-        <h1
-          style={{
-            textAlign: "center",
-            color: "red",
-            fontSize: "124px",
-          }}
-        >
-          Time-Table
-        </h1>
+      <Jumbotron className="main-jumbo">
+        <h1 id="main-h1">Time-Table</h1>
       </Jumbotron>
-      <Container style={{ border: "solid" }}>
+      <Container id="progress-container">
         <Jumbotron>
           <Card>
             <br />
             {/* Name of task will go here */}
-            <p style={{ margin: "auto" }}>Name of task</p>
-            <ProgressBar
-              variant="danger"
-              now={65}
-              style={{
-                height: "30px",
-                marginLeft: "10px",
-                marginRight: "10px",
-                marginBottom: "20px",
-              }}
-            />
+            <p className="task-name">Name of task</p>
+            <ProgressBar variant="danger" now={65} id="task-bar" />
             <Container>
               <Row>
                 <input
                   placeholder="Please enter an item to your list"
-                  style={{
-                    marginLeft: "10px",
-                    marginRight: "10px",
-                    marginBottom: "20px",
-                    width: "40%",
-                  }}
+                  id="input-task"
                 ></input>
-                <button style={{ marginLeft: "10px", height: "30px" }}>
-                  Add
-                </button>
+                <button className="input-task-button">Add</button>
                 <input
                   placeholder="Please enter a number of minutes for this task"
-                  style={{
-                    marginLeft: "20px",
-                    marginRight: "10px",
-                    marginBottom: "20px",
-                    width: "40%",
-                  }}
+                  id="input-task-minutes"
                 ></input>
-                <button
-                  style={{
-                    marginLeft: "10px",
-                    height: "30px",
-                  }}
-                >
-                  Add
-                </button>
+                <button className="input-task-button">Add</button>
               </Row>
             </Container>
           </Card>
           <br />
           <Card>
-            <p style={{ margin: "auto" }}>Transition</p>
-            <ProgressBar
-              variant="info"
-              now={20}
-              style={{
-                height: "30px",
-                marginTop: "25px",
-                marginLeft: "10px",
-                marginRight: "10px",
-                marginBottom: "20px",
-              }}
-            />
+            <p className="task-name">Transition</p>
+            <ProgressBar variant="info" now={20} id="transition-bar" />
             <Container>
               <Row>
                 <input
                   placeholder="Please enter a number of minutes for transition"
-                  style={{
-                    marginLeft: "520px",
-                    marginRight: "10px",
-                    marginBottom: "20px",
-                    width: "40%",
-                  }}
+                  id="input-transition-minutes"
                 ></input>
-                <button
-                  style={{
-                    marginLeft: "10px",
-                    height: "30px",
-                  }}
-                >
-                  Add
-                </button>
+                <button className="input-task-button">Add</button>
               </Row>
             </Container>
           </Card>
-          <button style={{ marginTop: "20px", marginLeft: "86%" }}>
-            Add another task
-          </button>
+          <button id="add-task-button">Add another task</button>
         </Jumbotron>
       </Container>
     </div>
-    // {/* // <div className="App">
-    // //   <div className="App-header">
-    // //     <h2>Welcome to React</h2>
-    // //   </div>
-    // //   <p className="App-intro">
-    // //     To get started, edit <code>src/App.js</code> and save to reload.
-    // //   </p>
-    // // </div> */}
   );
 }
 
