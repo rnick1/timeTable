cba36432ab5add908746d8b6697219435ab6fbfd
rnick1 <rnick0260@gmail.com>
2021-06-12 11:59:40 -0700

    Created Main.js and moved code out of App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,7 @@
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from "react";
-import { Card, Row, Container, Jumbotron, ProgressBar } from "react-bootstrap";
+import React from "react";
 import "./App.css";
+import Main from "./pages/Main.js";
 
-function App() {
-  return (
-    <div>
-      <Jumbotron className="main-jumbo">
-        <h1 id="main-h1">Time-Table</h1>
-      </Jumbotron>
-      <Container id="progress-container">
-        <Jumbotron>
-          <Card>
-            <br />
-            {/* Name of task will go here */}
-            <p className="task-name">Name of task</p>
-            <ProgressBar variant="danger" now={65} id="task-bar" />
-            <Container>
-              <Row>
-                <input
-                  placeholder="Please enter an item to your list"
-                  id="input-task"
-                ></input>
-                <button className="input-task-button">Add</button>
-                <input
-                  placeholder="Please enter a number of minutes for this task"
-                  id="input-task-minutes"
-                ></input>
-                <button className="input-task-button">Add</button>
-              </Row>
-            </Container>
-          </Card>
-          <br />
-          <Card>
-            <p className="task-name">Transition</p>
-            <ProgressBar variant="info" now={20} id="transition-bar" />
-            <Container>
-              <Row>
-                <input
-                  placeholder="Please enter a number of minutes for transition"
-                  id="input-transition-minutes"
-                ></input>
-                <button className="input-task-button">Add</button>
-              </Row>
-            </Container>
-          </Card>
-          <button id="add-task-button">Add another task</button>
-        </Jumbotron>
-      </Container>
-    </div>
-  );
+export default function App() {
+  return <Main />;
 }
-
-export default App;
